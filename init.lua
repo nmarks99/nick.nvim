e9b64c5f2b7a97fbc7769c711f566afdf4806b4f
Nick Marks <nmarks99@gmail.com>
2024-07-25 08:20:23 -0500

    minor settings changes

--- init.lua
+++ init.lua
@@ -2,25 +2,30 @@
 --            Options
 -- ==============================
 -- See `:help vim.o
-vim.g.mapleader = ' ' -- Set <space> as the leader key
+vim.g.mapleader = ' '           -- Set <space> as the leader key
 vim.g.maplocalleader = ' '
-vim.o.smartcase = true
-vim.o.hlsearch = true           -- Set highlight on search
+vim.o.hlsearch = false          -- Stops highlighting after enter
 vim.wo.number = true            -- enable line numbers
 vim.o.mouse = 'a'               -- Enable mouse mode
 vim.o.clipboard = 'unnamedplus' -- Sync clipboard
 vim.o.breakindent = true        -- Enable break indent
 vim.o.undofile = true           -- Save undo history
-vim.o.ignorecase = true         -- Case-insensitive search
+vim.o.ignorecase = true         -- Case-insensitive search...
+vim.o.smartcase = true          -- ...unless capital in query
 vim.wo.signcolumn = 'yes'       -- Keep signcolumn on by default
 vim.o.updatetime = 250          -- Decrease update time
 vim.o.timeoutlen = 300
+vim.opt.scrolloff = 8           -- scroll 8 lines before bottom
 vim.o.termguicolors = true      -- enable nice colors
 vim.opt.shiftwidth = 4          -- vim-slueth may change this
+vim.opt.belloff = "all"
+vim.opt.title = true
+vim.opt.incsearch = true
 vim.o.completeopt = 'menuone,noselect'
 default_theme = "catppuccin"
 default_transparency = false
 
+-- TODO: make this in another file, epics.lua?
 -- Disable autostart of the LSP for paths that contain these strings
 lsp_autostart_blacklist = { "APSshare", "iocBoot", "dserv" }
 
@@ -61,6 +66,13 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   pattern = '*',
 })
 
+-- keep highlighted when shifting with < and >
+vim.keymap.set("v", "<", "<gv" )
+vim.keymap.set("v", ">", ">gv" )
+
+-- Move stuff up and down and adjust indent
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 -- ==============================
 --            Plugins
