5b21251fb23333647edba47c302256d260be518a
Nick Marks <nmarks99@gmail.com>
2024-12-20 23:07:37 -0600

    remove leftover comment

--- init.lua
+++ init.lua
@@ -2,10 +2,3 @@ require("options")
 require("keymaps")
 require("plugins")
 require("theme")
-
--- -- TODO: rewrite theme.lua to do this:
--- local theme = os.getenv("NVIM_THEME")
--- local theme_transparent = os.getenv("NVIM_TRANSPARENT")
--- print(string.format("theme = %s", theme))
--- print(string.format("transparent = %s", theme_transparent))
-
