7a907c96ff8ec6229f519339244e72a35f0e901f
Nick Marks <nmarks99@gmail.com>
2025-08-11 23:50:12 -0500

    add diffview plugin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -16,6 +16,9 @@ local plugins = {
     { "lurst/austere.vim", lazy = false, priority = 1000},
     { "slugbyte/lackluster.nvim", lazy = false, priority = 1000},
 
+    -- view
+    { "sindrets/diffview.nvim" },
+
     -- fzf integration for neovim
     {
       "ibhagwan/fzf-lua",
